ee9529360a0f57471ce5cda842cafdfe3f134fb2
Brandon Cho <branyuncho@gmail.com>
2019-10-04 01:42:05 -0700

    2nd commit

--- app/src/main/java/com/example/class1/MainActivity.kt
+++ app/src/main/java/com/example/class1/MainActivity.kt
@@ -14,20 +14,46 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private var counter: Long = 0
+    var username: Any? = ""
     fun getStore() =  getPreferences(Context.MODE_PRIVATE)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        username = intent.extras?.get("username").toString()
+
+
+        // check to see if username is in shared preferences
+        // if not then add to shared preferences
+        // save counter to username
+        // can check shared preferences file
+        // View > Tool Windows > Device File Explorer > data > data > com.example.class1 > shared_prefs
+
         if (savedInstanceState != null){
+            updateCounter(savedInstanceState.getLong(username.toString(), 0))
+        }
+        else if (getStore().contains(username.toString())){
+            updateCounter(getStore().getLong(username.toString(), 0))
+        }
+
+        // Old
+        /*
+        if (savedInstanceState != null) {
+            updateCounter(savedInstanceState.getLong(COUNTER_KEY, 0))
+        }
+        else if (getStore().contains(COUNTER_KEY)) {
+            updateCounter(getStore().getLong(COUNTER_KEY, 0))
+        }*/
+        /*if (savedInstanceState != null){
             counter = savedInstanceState.getLong(COUNTER_KEY, 0)
             textCounter.text = counter.toString()
-        }
+        }*/
 
         myButton.setOnClickListener{
             counter++
-            textCounter.text = counter.toString()
+            updateCounter(counter)
 
             /*myImage.rotate90()
 
@@ -39,23 +65,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Attempt to update/change username later
+    /*
+    private fun updateUsername(name: String){
+        username = name
+    }*/
+
+    private fun updateCounter(count: Long){
+        counter = count
+        textCounter.text = counter.toString()
+    }
+
     override fun onPause() {
         super.onPause()
-        getStore().edit().putLong(COUNTER_KEY, counter).apply()
+        getStore().edit().putLong(username.toString(), counter).apply()
 
     }
 
-    override fun onSaveInstanceState(outState: Bundle?) {
+    override fun onSaveInstanceState(outState: Bundle) {
         outState?.run {
-            putLong(COUNTER_KEY, counter)
+            putLong(username.toString(), counter)
         }
 
         super.onSaveInstanceState(outState)
     }
 
+    /* I don;t think I need this anymore
     companion object {
         private const val COUNTER_KEY = "counterKey"
-    }
+    }*/
 
 
 }
